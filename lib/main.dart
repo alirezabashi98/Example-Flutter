c8cf9b4706df416da02a39411799ff4209a6a1b0
alirezabashi98 <alirezabashi98@gmail.com>
2022-03-14 17:40:54 +0330

    add OutlinedButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,72 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Application());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(fontFamily: 'vazir'),
+        home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'vazir'),
-      home: Scaffold(
-        body: _getMainBody(),
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'خوش آمدید',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                    color: Colors.white),
+              ),
+              Image(
+                image: AssetImage('images/splash_screen.png'),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: Text('ثبت نام'),
+                style: OutlinedButton.styleFrom(
+                  minimumSize: Size(200.0, 40.0),
+                  side: BorderSide(color: Colors.white,width: 2.0),
+                  primary: Colors.white
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
+}
 
-  Widget _getMainBody(){
-    return SafeArea(child: Text('Hello world'),);
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
   }
-
 }
