679bb8cd6647c93a43712fca92b03b347e46c628
alirezabashi98 <alirezabashi98@gmail.com>
2022-03-15 05:24:23 +0330

    make login page add icon add button login & sing in & scroll page

--- lib/main.dart
+++ lib/main.dart
@@ -26,32 +26,58 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: Colors.blue,
       body: Center(
         child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'خوش آمدید',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0,
-                    color: Colors.white),
+          child: SingleChildScrollView(
+            child:  Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'خوش آمدید',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                            color: Colors.white),
+                      ),
+                      SizedBox(width: 14,),
+                      Icon(Icons.login,color: Colors.white,)
+                    ],
+                  ),
+                  Image(
+                    image: AssetImage('images/splash_screen.png'),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'ورود به حساب',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                        minimumSize: Size(200.0, 40.0),
+                        side: BorderSide(color: Colors.white, width: 2.0),
+                        primary: Colors.white),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        primary: Colors.black,
+                        minimumSize: Size(200.0, 40.0)),
+                    onPressed: () {},
+                    child: Text(
+                      'ثبت نام',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ],
               ),
-              Image(
-                image: AssetImage('images/splash_screen.png'),
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                child: Text('ثبت نام'),
-                style: OutlinedButton.styleFrom(
-                  minimumSize: Size(200.0, 40.0),
-                  side: BorderSide(color: Colors.white,width: 2.0),
-                  primary: Colors.white
-                ),
-              ),
-            ],
+            ),
           ),
         ),
-      ),
     );
   }
 }
